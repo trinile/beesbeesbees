2d9e66ed677bbc0e4ea0d2313fcd954ba82ccaaf
Trini Le <trinile@gmail.com>
2016-03-07 11:24:04 -0800

    ForagerBee complete, pass specs

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+  HoneyMakerBee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.treasureChest = [];
+  this.canFly = true;
 };
 
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(value) {
+  this.treasureChest.push(value);
+}
